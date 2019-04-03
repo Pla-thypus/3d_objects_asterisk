b61597e309d415fea04e99a072de210a8c54bf65
Pla-thypus <42321342+Pla-thypus@users.noreply.github.com>
2019-04-04 01:40:11 +0200

    Add files via upload

---
+++ Taper_V1_store/sketch.js
@@ -0,0 +1,53 @@
+var scan;
+let angle = 0;
+var li =100;
+var canvas;
+
+
+
+function setup() {
+  
+  canvas = createCanvas(315, 315, WEBGL);
+  
+  
+  scan = loadModel('taper_V2.obj');
+
+  frameRate(30);
+  angleMode(DEGREES);
+  
+
+}
+
+function draw() {
+  
+  background(0);
+
+  translate(0,0,0);
+  orbitControl(3);
+  smooth();
+  
+  
+  scale(0.9);
+  
+  specularMaterial(255)
+  noStroke()
+  //stroke(255);
+  //noFill();
+  
+  ambientLight(110);
+  directionalLight(li,li,li,1,0.5,-1);
+  directionalLight(li,li,li,-1,1,1);
+  directionalLight(li,li,li,-1,-1,-1);
+  
+  
+  //rotateX(angle * 0.5);
+  rotateY(angle * 0.5);
+  //rotateZ(angle * 0.2);
+
+  model(scan);
+  
+  
+  angle += -0.4;
+  
+
+}
